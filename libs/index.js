9994750487a5341625051f3e5372669c372f2b90
xgqfrms <xgqfrms@outlook.com>
2019-02-25 10:42:18 +0800

    Update index.js

--- libs/index.js
+++ libs/index.js
@@ -7,15 +7,21 @@
 const fetch = require('node-fetch');
 const showdata = require('./showdata');
 
+// NODE_ENV_ID = `08ecc2f68d922f18800e`;
+// NODE_ENV_SECRET = `5846d428b5340812b76c9637eceaee979340b922`;
+
 const nct = (username, repo) => {
-    fetch(`https://api.github.com/repos/${username}/${repo}/commits`,{
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+    let options = {
         data: {
-            client_id: '08ecc2f68d922f18800e',
-            client_secret: '5846d428b5340812b76c9637eceaee979340b922'
-        }
-    })
-    .then((response) => response.json())
+            client_id: `08ecc2f68d922f18800e`,
+            client_secret: `5846d428b5340812b76c9637eceaee979340b922`,
+        },
+    };
+    fetch(url, options)
+    .then((res) => res.json())
     .then((json)=> {
+        // not need at all
         return repos = json;
     })
     .then((repos)=>{
